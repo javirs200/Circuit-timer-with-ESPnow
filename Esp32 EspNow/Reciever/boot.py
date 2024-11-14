c127251c33915a7af14c1822d54aec3617513846
javi <fordrsmax@gmail.com>
2024-11-14 20:38:38 +0100

    add com messages and time diference calculation

--- Esp32 EspNow/Reciever/boot.py
+++ Esp32 EspNow/Reciever/boot.py
@@ -4,6 +4,8 @@ import espnow
 from machine import Pin
 from utime import sleep
 
+print("Reciever booting...")
+
 led=Pin(2,Pin.OUT) # onboard led
 led.value(0)
 
@@ -14,6 +16,8 @@ def flash(speed):
         led.value(0)
         sleep(speed)
 
+print("Setting up network...")
+
 # A WLAN interface must be active to send()/recv()
 sta = network.WLAN(network.STA_IF)
 sta.active(True)
@@ -22,11 +26,25 @@ sta.disconnect()   # Because ESP8266 auto-connects to last Access Point
 e = espnow.ESPNow()
 e.active(True)
 
+currentLap = 0
+dbTreshold = -50
+
+print("starting to recieve lap ping")
+
 while True:
-    host, msg = e.recv()
+    host,msg = e.recv()
     if msg: # msg == None if timeout in recv()
         if msg == b'lap':
-            db = e.peers_table[host][0]
-            # print(host,msg)
-            print(db)
             flash(0.1)
+            currentmsgData = e.peers_table[host]
+            db = currentmsgData[0]
+            timestamp = currentmsgData[1]
+            if db > dbTreshold:
+                if currentLap == 0:
+                    currentLap = timestamp
+                    print("initial lap")
+                elif timestamp - currentLap > 1000:
+                    print("time:", timestamp - currentLap)
+                    flash(0.2)
+                currentLap = timestamp
+            
