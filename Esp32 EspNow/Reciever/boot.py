ec90af17389404a7148ecf0e64f9555d93adf3ae
javi <fordrsmax@gmail.com>
2024-11-15 16:32:37 +0100

    add stdin read non-bloking and uasyncio

--- Esp32 EspNow/Reciever/boot.py
+++ Esp32 EspNow/Reciever/boot.py
@@ -1,10 +1,11 @@
 import network
 import espnow
+import sys
+import uasyncio
+import uselect
 
 from machine import Pin
 from utime import sleep
-
-print("Reciever booting...")
 
 led=Pin(2,Pin.OUT) # onboard led
 led.value(0)
@@ -16,35 +17,74 @@ def flash(speed):
         led.value(0)
         sleep(speed)
 
-print("Setting up network...")
+async def serialManager(dbTreshold):
+    count = 0
+    msg = ""
+    while True:
+        # read byte(str) from stdin non-blocking
+        list = uselect.select([sys.stdin], [], [], 0.01)
+        if list[0]:
+            char = sys.stdin.read(1)
+        else:
+            char = None
+        
+        # build msg
+        if char:
+            msg = msg + char
+            count += 1
+            if char == '\n':
+                print("msg:",msg , "count:",count)
+                if count == 4:
+                    newTreshold = int(msg)
+                    # print("newTreshold:",newTreshold)
+                    if newTreshold < 0 and newTreshold != dbTreshold[0]:
+                        dbTreshold[0] = newTreshold
+                        # print("new dbTreshold:",dbTreshold[0])
+                count = 0
+                msg = ""    
+                
+        await uasyncio.sleep(0.2)
 
-# A WLAN interface must be active to send()/recv()
-sta = network.WLAN(network.STA_IF)
-sta.active(True)
-sta.disconnect()   # Because ESP8266 auto-connects to last Access Point
-
-e = espnow.ESPNow()
-e.active(True)
-
-currentLap = 0
-dbTreshold = -50
-
-print("starting to recieve lap ping")
-
-while True:
-    host,msg = e.recv()
-    if msg: # msg == None if timeout in recv()
+async def espnowManager(e,dbTreshold,currentLap):
+    while True:
+        host,msg = e.recv()
         if msg == b'lap':
             flash(0.1)
             currentmsgData = e.peers_table[host]
             db = currentmsgData[0]
             timestamp = currentmsgData[1]
-            if db > dbTreshold:
+            # print("db:",db,"dbTreshold:",dbTreshold[0])
+            if db > dbTreshold[0]:
                 if currentLap == 0:
-                    currentLap = timestamp
                     print("initial lap")
                 elif timestamp - currentLap > 1000:
                     print("time:", timestamp - currentLap)
                     flash(0.2)
                 currentLap = timestamp
-            
+        await uasyncio.sleep(0.2)
+
+def main():
+    # A WLAN interface must be active to send()/recv()
+    sta = network.WLAN(network.STA_IF)
+    sta.active(True)
+    sta.disconnect()   # Because ESP8266 auto-connects to last Access Point
+
+    e = espnow.ESPNow()
+    e.active(True)
+
+    currentLap = 0
+    dbTreshold = [-50]
+
+    try:
+        loop = uasyncio.get_event_loop()
+        loop.create_task(serialManager(dbTreshold))
+        loop.create_task(espnowManager(e,dbTreshold,currentLap))
+        loop.run_forever()
+    except Exception as e:
+        print('Exception ',e)
+        loop.stop()
+        loop.close()
+
+if __name__ == "__main__":
+    main()
+        
